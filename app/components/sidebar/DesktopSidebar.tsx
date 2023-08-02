6f7597c439003d66e80863aa1ed14b836f27bd3b
Hosein Emdadi <hemdadi71@gmail.com>
2023-08-02 20:19:34 +0430

    chat icons and user image placeholder completed

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,15 @@
 import useRoutes from '@/app/hooks/useRoutes'
 import { useState } from 'react'
 import DesktopItem from './DesktopItem'
+import { User } from '@prisma/client'
+import Avatar from '../Avatar'
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes()
-  console.log(routes)
   const [isOpen, setIsOpen] = useState(false)
   return (
     <>
@@ -24,6 +29,13 @@ const DesktopSidebar = () => {
               />
             ))}
           </ul>
+        </nav>
+        <nav className="mt-4 flex flex-col justify-center items-center">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="cursor-pointer hover:opacity-75 transition">
+            <Avatar user={currentUser} />
+          </div>
         </nav>
       </div>
     </>
